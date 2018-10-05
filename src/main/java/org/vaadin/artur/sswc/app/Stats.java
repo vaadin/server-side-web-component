4b00566ac887024d89c03d4462db9ff8149f7408
Artur Signell <artur@vaadin.com>
2018-10-05 13:01:20 +0300

    Add a second component

---
+++ src/main/java/org/vaadin/artur/sswc/app/Stats.java
@@ -0,0 +1,59 @@
+package org.vaadin.artur.sswc.app;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
+
+import org.apache.commons.io.IOUtils;
+import org.vaadin.artur.sswc.internal.WebComponent;
+
+import com.vaadin.flow.component.html.Div;
+
+@WebComponent("server-stats")
+public class Stats extends Div {
+
+    public Stats() {
+        update();
+        getElement().addEventListener("click", e -> {
+            update();
+        });
+    }
+
+    public void update() {
+        String processes;
+        try {
+            Runtime runtime = Runtime.getRuntime();
+            processes = IOUtils
+                    .toString(runtime.exec("ps ax").getInputStream());
+            String processCount = processes.replaceAll("[^\\n]", "").length()
+                    + "";
+            String text = "Server status (click to update)";
+            text += "<br>";
+            text += "<br>";
+            text += "Processes running: " + processCount;
+            text += "<br>";
+            text += "Free memory: " + format(runtime.freeMemory());
+            text += "<br>";
+            text += "Last updated: "
+                    + DateFormat.getDateTimeInstance().format(new Date());
+
+            setInnerHtml(text);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private String format(long freeMemory) {
+        return freeMemory / 1024 / 1024 + "MB";
+    }
+
+    private void setInnerHtml(String html) {
+        // Workaround for https://github.com/vaadin/flow/issues/4644
+        String current = getElement().getProperty("innerHTML");
+        if (!html.equals(current)) {
+            getElement().setProperty("innerHTML", html);
+        }
+    }
+
+}
